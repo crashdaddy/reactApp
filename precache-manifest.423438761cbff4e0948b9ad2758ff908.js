da20d9a5fe8619969ebd4129bc22e43f9be6aea0
crashdaddy <kennethscoggins@mail.com>
2020-04-29 20:05:39 -0500

    initial commit

---
+++ precache-manifest.423438761cbff4e0948b9ad2758ff908.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b11c822de5374ad14cdd3a711f4fb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd064f4feedc4dcecb2e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "37953da5eee6e0fff11a",
+    "url": "/static/js/2.b08bb56f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b08bb56f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd064f4feedc4dcecb2e",
+    "url": "/static/js/main.1657aae7.chunk.js"
+  },
+  {
+    "revision": "a7e400943f253bf6014a",
+    "url": "/static/js/runtime-main.88e1b363.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
